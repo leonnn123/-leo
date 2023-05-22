7de7d155d1fe3a5cdcaf4a4c1a1796714f64ac89
leonnn123 <133523698+leonnn123@users.noreply.github.com>
2023-05-22 12:00:53 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+MakeyMakey.setSimulatorKeymap(
+MakeyMakey.PlayerNumber.ONE,
+MakeyMakey.MakeyMakeyKey.UP,
+MakeyMakey.MakeyMakeyKey.UP,
+MakeyMakey.MakeyMakeyKey.UP,
+MakeyMakey.MakeyMakeyKey.UP,
+MakeyMakey.MakeyMakeyKey.W,
+MakeyMakey.MakeyMakeyKey.D
+)
